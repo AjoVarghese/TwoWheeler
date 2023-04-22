0013eacf21e41c4643f3ca6cbf148952d4970f8f
Ajo <ajovarghese987@gmail.com>
2023-04-22 18:19:33 +0530

    pagination bug fixed

--- server/Controller/User/Vehicles/VehicleStoreController.js
+++ server/Controller/User/Vehicles/VehicleStoreController.js
@@ -62,31 +62,5 @@ exports.viewVehicles = async(req,res) => {
   }
 }
 
-exports.blabla = async(req,res) => {
-  
-  try {
-     vehicleSchema.find(
-      {Location : req.body.Location}
-     ).sort({Price : 1}).then((data) => {
-      res.status(200).json(data)
-     })
-  } catch (error) {
-    
-  }
-}
 
-//for Pagination
-exports.getAllBikes = async(req,res) => {
-  vehicleSchema.find({Status : "Acccepted"}).then(async(data) => {
-    const page = parseInt(req.query.page) || 1
-    const pageSize = parseInt(req.query.limit) || 2
-    const skip = (page - 1) * pageSize
 
-    const pages = Math.ceil(data.length / pageSize)
-
-    data = data.skip(skip).limit(pageSize)
-  })
-  .catch((err) => {
-
-  })
-}
