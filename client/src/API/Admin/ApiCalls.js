ce60baefe99822520bc597cdd7dd8c8ddc18816f
Ajo <ajovarghese987@gmail.com>
2023-05-01 09:35:08 +0530

    code formatted

--- client/src/API/Admin/ApiCalls.js
+++ client/src/API/Admin/ApiCalls.js
@@ -1,66 +1,99 @@
-import axios from "axios"
-// let token = user?.token
-const API = axios.create({baseURL : "http://localhost:3001/api/admin"})
+import axios from "axios";
 
-const admin = JSON.parse(localStorage.getItem('adminInfo'))
-console.log("Admin Config",admin?admin:"");
+const API = axios.create({ baseURL: process.env.REACT_APP_ADMIN_BASE_URL });
+
+const admin = JSON.parse(localStorage.getItem("adminInfo"));
+console.log("Admin Config", admin ? admin : "");
 
 const config = {
-    headers : {
-      "Content-Type" : "application/json",
-    }
-}
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
 const configToken = {
-  headers : {
-   "Content-Type" : "application/json",
-   Authorization:"Bearer"+' '+admin?.token   
-  }
-}
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer" + " " + admin?.token,
+  },
+};
 
 const configFormData = {
-  headers : {
+  headers: {
     "Content-Type": "multipart/form-data",
-    Authorization:"Bearer"+' '+admin?.token   
-  }
-}
+    Authorization: "Bearer" + " " + admin?.token,
+  },
+};
 
-export const adminLoginApi = (Email,Password) => API.post('/login',{Email,Password},config)
-export const getUsersApi = (page) => API.get(`/users?page=${page}`,configToken)
-export const blockUnblockApi = (id) => API.get('/blockUnblock?id=' +id,configToken)
-export const adminAddBikeApi = (formData) => API.post('/add-bikes',formData,configFormData)
-export const getAllBikesApi = (page) => API.get(`/bikes?page=${page}`,configToken)
-export const bikeSingleViewApi = (id) => API.get('/bike-detailed-view?id='+id , configToken)
-export const getRentRequetsApi = () => API.get('/rent-requests',configToken)
-export const acceptRentRequestsApi = (id,owner) => API.get(`/accept-request?id=${id}&owner=${owner}`,configToken)
-export const rejectRentRequetsApi = (id) => API.get('/reject-requests?id='+id,configToken)
-export const deleteBikeAPi =(id) => API.get('/delete-bike?id='+id,configToken)
-export const editBikeApi = (id,formData) => API.post('/edit-bike?id='+id,formData,configFormData)
+// login
+export const adminLoginApi = (Email, Password) =>
+  API.post("/login", { Email, Password }, config);
 
-export const adminSearchBikeApi = (searchTerm,page)=> {
-  return(
-   API.post(`/search-bikes?page=${page}`,{searchTerm},configToken)
-  )
-}
+  // users
+export const getUsersApi = (page) =>
+  API.get(`/users?page=${page}`, configToken);
+export const blockUnblockApi = (id) =>
+  API.get("/blockUnblock?id=" + id, configToken);
 
-export const getLocationApi = () => API.get('/locations',configToken)
-export const addLocationApi = (location) => API.post('/add-location',{location},configToken)
-export const deleteLocationApi = (id) => API.get('/delete-location?id='+id,configToken)
-export const editLocationApi = (id,location) => API.post('/edit-location?id='+id,{location})
+  // bikes
+export const adminAddBikeApi = (formData) =>
+  API.post("/add-bikes", formData, configFormData);
+export const getAllBikesApi = (page) =>
+  API.get(`/bikes?page=${page}`, configToken);
+export const bikeSingleViewApi = (id) =>
+  API.get("/bike-detailed-view?id=" + id, configToken);
 
-export const addCouponApi = (couponName,couponCode,couponPrice) => API.post('/add-coupon',{couponName,couponCode,couponPrice},configToken)
-export const getCouponsApi = () => API.get('/coupons',configToken)
-export const editCouponApi = (id,couponName,couponCode) => API.post('/edit-coupon?id='+id,{couponName,couponCode},configToken)
-export const deleteCouponApi = (id) => API.delete('/delete-coupon?id='+id,configToken)
+  // rent-requests
+export const getRentRequetsApi = () => API.get("/rent-requests", configToken);
+export const acceptRentRequestsApi = (id, owner) =>
+  API.get(`/accept-request?id=${id}&owner=${owner}`, configToken);
+export const rejectRentRequetsApi = (id) =>
+  API.get("/reject-requests?id=" + id, configToken);
 
-export const getBookedDetailsApi = () => API.get('/booking-details',configToken)
+  //bike-actions
+export const deleteBikeAPi = (id) =>
+  API.get("/delete-bike?id=" + id, configToken);
+export const editBikeApi = (id, formData) =>
+  API.post("/edit-bike?id=" + id, formData, configFormData);
 
-export const getPendingBookingsApi = () => API.get('/get-pending-bookings',configToken)
+export const adminSearchBikeApi = (searchTerm, page) => {
+  return API.post(`/search-bikes?page=${page}`, { searchTerm }, configToken);
+};
 
-export const getOnRideBookingsApi = () => API.get('/get-onRide-bookings',configToken)
+//location
+export const getLocationApi = () => API.get("/locations", configToken);
+export const addLocationApi = (location) =>
+  API.post("/add-location", { location }, configToken);
+export const deleteLocationApi = (id) =>
+  API.get("/delete-location?id=" + id, configToken);
+export const editLocationApi = (id, location) =>
+  API.post("/edit-location?id=" + id, { location });
 
-export const cancelledBookingsApi = () => API.get('/get-cancelled-bookings',configToken)
+  //coupons
+export const addCouponApi = (couponName, couponCode, couponPrice) =>
+  API.post("/add-coupon", { couponName, couponCode, couponPrice }, configToken);
+export const getCouponsApi = () => API.get("/coupons", configToken);
+export const editCouponApi = (id, couponName, couponCode) =>
+  API.post("/edit-coupon?id=" + id, { couponName, couponCode }, configToken);
+export const deleteCouponApi = (id) =>
+  API.delete("/delete-coupon?id=" + id, configToken);
 
-export const getDashBoardDetailsApi = () => API.get('get-dashboard-details',configToken)
+  //booking-details
+export const getBookedDetailsApi = () =>
+  API.get("/booking-details", configToken);
 
-export const getSalesReportApi = () => API.get('/get-salesReport',configToken)
+export const getPendingBookingsApi = () =>
+  API.get("/get-pending-bookings", configToken);
+
+export const getOnRideBookingsApi = () =>
+  API.get("/get-onRide-bookings", configToken);
+
+export const cancelledBookingsApi = () =>
+  API.get("/get-cancelled-bookings", configToken);
+
+  //dashboard
+export const getDashBoardDetailsApi = () =>
+  API.get("get-dashboard-details", configToken);
+
+  //sales-report
+export const getSalesReportApi = () => API.get("/get-salesReport", configToken);
--- client/src/API/User/ApiCalls.js
+++ client/src/API/User/ApiCalls.js
@@ -1,93 +1,139 @@
-import axios from "axios"
-const API = axios.create({baseURL : "http://localhost:3001/api/user"})
+import axios from "axios";
+const API = axios.create({ baseURL: process.env.REACT_APP_CLIENT_BASE_URL });
 
 const config = {
-    headers : {
-      "Content-Type" : "application/json",
-    }
-}
-const user = JSON.parse(localStorage.getItem('userInfo'))
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+const user = JSON.parse(localStorage.getItem("userInfo"));
 
-const ID = user?.id
+const ID = user?.id;
 
 const configTOken = {
-  headers : {
-   "Content-Type" : "application/json",
-   Authorization:"Bearer"+' '+user?.token   
-  }
-}
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer" + " " + user?.token,
+  },
+};
 
 const configFormData = {
-  headers : {
+  headers: {
     "Content-Type": "multipart/form-data",
-    Authorization:"Bearer"+' '+user?.token   
-  }
-}
+    Authorization: "Bearer" + " " + user?.token,
+  },
+};
 
-export const googleSignupApi = (Name,Email,Phone,Photo) => API.post('/google-signup',{Name,Email,Phone,Photo},config)
-export const userSignupApi = (Name,Email,Mobile,Password,Referral) => API.post('/signup',{Name,Email,Mobile,Password,Referral},config)
-export const userLoginAPi = (Mobile,Password) => API.post('/login',{Mobile,Password},config)
+//signup-login
+export const googleSignupApi = (Name, Email, Phone, Photo) =>
+  API.post("/google-signup", { Name, Email, Phone, Photo }, config);
 
-export const OTPLoginApi = (mobile) => API.post('/otp-login',{mobile},config)
+export const userSignupApi = (Name, Email, Mobile, Password, Referral) =>
+  API.post("/signup", { Name, Email, Mobile, Password, Referral }, config);
 
-export const userHomeApi = () => API.get('/',config)
+export const userLoginAPi = (Mobile, Password) =>
+  API.post("/login", { Mobile, Password }, config);
 
-export const getHomeBikesActionApi = () => API.get('/home-bikes',config)
+export const OTPLoginApi = (mobile) =>
+  API.post("/otp-login", { mobile }, config);
 
-export const userProfileApi = (id) =>API.get('/profile?id='+id,configTOken)
-export const imageUploadApi = (id,image) => API.post('/profileImageUpdate?id='+id,{image},configTOken)
-export const editProfileApi = (name,email,mobile,id) => {
-  return(
-      API.post('/edit-profile?id='+id,{name,email,mobile},configTOken)
-  )
-}
 
-export const userGetLocationApi = () => API.get('/get-location',config)
+  //home
+export const userHomeApi = () => API.get("/", config);
 
-export const userAddBikeApi = (formData)=> API.post('/rent-bikes?id='+ID,formData,configFormData)
-export const userGetRentedBikesAPi = () => API.get('/rented-bikes?id='+ID,configTOken)
+//bikes
+export const getHomeBikesActionApi = () => API.get("/home-bikes", config);
 
-export const userGetBikesApi = (page) => API.get(`/bikes?id=${ID}&page=${page}`,config)
-export const searchBikesApi = (searchTerm,page) => {
-   return(
-    API.post(`/search-bikes?page=${page}&id=${ID}`,{searchTerm},config)
-   )
-}
+//profile
+export const userProfileApi = (id) => API.get("/profile?id=" + id, configTOken);
+export const imageUploadApi = (id, image) =>
+  API.post("/profileImageUpdate?id=" + id, { image }, configTOken);
+export const editProfileApi = (name, email, mobile, id) => {
+  return API.post(
+    "/edit-profile?id=" + id,
+    { name, email, mobile },
+    configTOken
+  );
+};
 
-export const filterBikesApi = (location,brand,page) => API.post(`/filter-bikes?page=${page}`,{location,brand},config)
+//location
+export const userGetLocationApi = () => API.get("/get-location", config);
 
-export const bookBikeApi = (bookingData) => API.post('/bike-booking',{bookingData},config)
+//rent-bikes
+export const userAddBikeApi = (formData) =>
+  API.post("/rent-bikes?id=" + ID, formData, configFormData);
+export const userGetRentedBikesAPi = () =>
+  API.get("/rented-bikes?id=" + ID, configTOken);
 
-export const createOrderApi = (bookingDetails) => API.post('/booking-success',{bookingDetails},config)
+  //bikes
+export const userGetBikesApi = (page) =>
+  API.get(`/bikes?id=${ID}&page=${page}`, config);
+export const searchBikesApi = (searchTerm, page) => {
+  return API.post(
+    `/search-bikes?page=${page}&id=${ID}`,
+    { searchTerm },
+    config
+  );
+};
 
-export const rentedRidesApi = () => API.get('/my-rents?id='+ID,configTOken)
+export const filterBikesApi = (location, brand, page) =>
+  API.post(`/filter-bikes?page=${page}`, { location, brand }, config);
 
-export const cancelRideApi = (bikeId,bookingId,startTime,endTime,userId,price) =>
- API.get(`/cancel-ride?bikeId=${bikeId}&bookingId=${bookingId}
+  //booking
+export const bookBikeApi = (bookingData) =>
+  API.post("/bike-booking", { bookingData }, config);
+
+export const createOrderApi = (bookingDetails) =>
+  API.post("/booking-success", { bookingDetails }, config);
+
+  //rent-details
+export const rentedRidesApi = () => API.get("/my-rents?id=" + ID, configTOken);
+
+export const cancelRideApi = (
+  bikeId,
+  bookingId,
+  startTime,
+  endTime,
+  userId,
+  price
+) =>
+  API.get(
+    `/cancel-ride?bikeId=${bikeId}&bookingId=${bookingId}
  &startTime=${startTime}&endTime=${endTime}&userId=${userId}
- &price=${price}`,configTOken)
+ &price=${price}`,
+    configTOken
+  );
 
- export const endRideApi = (bikeId,bookingId,startTime,endTime,userId) =>
- API.get(`/end-ride?bikeId=${bikeId}
+export const endRideApi = (bikeId, bookingId, startTime, endTime, userId) =>
+  API.get(
+    `/end-ride?bikeId=${bikeId}
  &bookingId=${bookingId}&startTime=${startTime}
- &endTime=${endTime}&userId=${userId}`,configTOken)
-
- export const payFineApi = (fineDetails) => API.post('/pay-fine?id='+ID,{fineDetails},configTOken)
-
- export const paymentSuccessApi =(fineDetails) => API.post('/payment-success',{fineDetails},config)
+ &endTime=${endTime}&userId=${userId}`,
+    configTOken
+  );
 
 
-export const getWalletApi = () => API.get('/get-wallet?id='+ID,configTOken)
+  //fine
+export const payFineApi = (fineDetails) =>
+  API.post("/pay-fine?id=" + ID, { fineDetails }, configTOken);
+
+export const paymentSuccessApi = (fineDetails) =>
+  API.post("/payment-success", { fineDetails }, config);
+
+  //wallet
+export const getWalletApi = () => API.get("/get-wallet?id=" + ID, configTOken);
 
 // chat
-export const getAllUserContacts = (id) => API.get("/contacts?id=" + id, configTOken)
-export const sendMessageAPI = (data) => API.post("/add-message", { data }, configTOken)
-export const getAllMessagesAPI = (data) => API.post("/get-all-messages", { data }, configTOken)
+export const getAllUserContacts = (id) =>
+  API.get("/contacts?id=" + id, configTOken);
+export const sendMessageAPI = (data) =>
+  API.post("/add-message", { data }, configTOken);
+export const getAllMessagesAPI = (data) =>
+  API.post("/get-all-messages", { data }, configTOken);
 
-export const getAllOwnersApi = () => API.get('/get-owners?id='+ID,configTOken)
+export const getAllOwnersApi = () =>
+  API.get("/get-owners?id=" + ID, configTOken);
 
-export const sendMessageAPi = (data) => API.post('/send-message',{data})
+export const sendMessageAPi = (data) => API.post("/send-message", { data });
 
-export const imageSendApi = (data) => API.post('/send-image',{data})
-
-
+export const imageSendApi = (data) => API.post("/send-image", { data });
